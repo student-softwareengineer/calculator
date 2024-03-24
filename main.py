7291fe6dade1811d88d84e1a2ba8a7c0ac6f106d
student-softwareengineer <mehboobzahid558@gmail.com>
2024-03-23 20:45:41 -0700

    add sum function
    this is for testing branch and PR

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def sum_fun(a: int, b: int) -> int:
+    return a + b
+
+
 num_1 = int(input("Enter First Number =>"))
 num_2 = int(input("Enter Second Number =>"))
 
-result = num_1 + num_2
+result = sum_fun(a=num_1, b=num_2)
 
 print(result)
