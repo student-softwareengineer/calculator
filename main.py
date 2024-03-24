f3c4591cf4714951a31ad102e1beeb574f9ea008
student-softwareengineer <mehboobzahid558@gmail.com>
2024-03-23 21:08:44 -0700

    add multipy function

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def sum_fun(a: int, b: int) -> int:
     return a + b
 
 
+def multiply_fun(a: int, b: int) -> int:
+    return a * b
+
+
 num_1 = int(input("Enter First Number =>"))
 num_2 = int(input("Enter Second Number =>"))
 
