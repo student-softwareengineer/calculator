20ce989c8e99d6842cf9226dbf99821593b75f45
student-softwareengineer <mehboobzahid558@gmail.com>
2024-03-23 20:52:52 -0700

    change variable

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ def sum_fun(a: int, b: int) -> int:
 num_1 = int(input("Enter First Number =>"))
 num_2 = int(input("Enter Second Number =>"))
 
-result = sum_fun(a=num_1, b=num_2)
+sum_result = sum_fun(a=num_1, b=num_2)
 
-print(result)
+print(sum_result)
